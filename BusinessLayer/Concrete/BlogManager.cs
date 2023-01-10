9db61faf084bfd3edfdf95f14a6d1661a2d37ba4
Onurcan Şimşek <onurcan.25.1999@gmail.com>
2023-01-10 12:19:27 +0300

    Add project files.

---
+++ BusinessLayer/Concrete/BlogManager.cs
@@ -0,0 +1,48 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+	public class BlogManager : IBlogService
+	{
+		IBlogDal _blogdal;
+
+		public BlogManager(IBlogDal blogdal)
+		{
+			_blogdal = blogdal;
+		}
+
+		
+		public void BlogDelete(Blog blog)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void BloAdd(Blog blog)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void BlogUpdate(Blog blog)
+		{
+            throw new NotImplementedException();
+
+        }
+
+		public Blog GetById(int id)
+		{
+            throw new NotImplementedException();
+        }
+
+		public List<Blog> GetList()
+		{
+			return _blogdal.GetListAll();
+		}
+	}
+}
